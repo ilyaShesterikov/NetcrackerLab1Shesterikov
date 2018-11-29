388c1755a921612c7428065a2a24abddba80947d
ilyaShesterikov <shesterikov9@gmail.com>
2018-11-29 17:56:08 +0200

    1000 to constant

--- Lab1/src/main/java/output/Output.java
+++ Lab1/src/main/java/output/Output.java
@@ -7,10 +7,11 @@ import analyzer.Analyze.SortType;
 
 public class Output {
     public static void main(String[] args) {
+        int arraySize = 1000;
         for(FillType ft: FillType.values()){
             for (SortType st : SortType.values()) {
                 Analyze a = new Analyze();
-                long time = a.analyze(a.makeArray(ft, 1000), st);
+                long time = a.analyze(a.makeArray(ft, arraySize), st);
                 System.out.println("FillType: " + ft + "\tSortType: " + st + "\t" + time);
             }
         }
