5a3cfeb1217215a1aeb6ae77249c90c714e23fad
ilyaShesterikov <shesterikov9@gmail.com>
2018-12-17 11:10:11 +0200

    found common in bubblesorters

--- Lab1/src/main/java/sorters/BubbleSortFloatUp.java
+++ Lab1/src/main/java/sorters/BubbleSortFloatUp.java
@@ -3,13 +3,9 @@ package sorters;
 public class BubbleSortFloatUp extends BubbleSorter {
     public void sort(int[] array) {
         int n = array.length;
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n-1; i++){
             for (int j = n - 1; j > i; j--){
-                if (array[j-1] > array[j]){
-                    int temp = array[j];
-                    array[j] = array[j-1];
-                    array[j-1] = temp;
-                }
+                checkAndSwap(array, j-1);
             }
         }
     }
--- Lab1/src/main/java/sorters/BubbleSorter.java
+++ Lab1/src/main/java/sorters/BubbleSorter.java
@@ -1,5 +1,11 @@
 package sorters;
 
 public abstract class BubbleSorter extends AbstractSorter {
-
+    protected void checkAndSwap(int[] array, int firstPosition){
+        if (array[firstPosition] > array[firstPosition+1]) {
+            int temp = array[firstPosition];
+            array[firstPosition] = array[firstPosition+1];
+            array[firstPosition+1] = temp;
+        }
+    }
 }
